68ef9862f1b0daaf7d63220ae5ec29db74b7b7c0
morlabhaskar <morlabhaskar306@gmail.com>
2024-07-31 15:22:17 +0530

    modified

--- src/api/Url.js
+++ src/api/Url.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:4000"
-// export const API_URL = "https://swiggy-mern-stack-server.onrender.com"
-export const API_URL = "https://swiggy-mern-stack-server.vercel.app"
+export const API_URL = "https://swiggy-mern-stack-server.onrender.com"
+// export const API_URL = "https://swiggy-mern-stack-server.vercel.app"
